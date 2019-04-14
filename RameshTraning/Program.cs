ba9983820f884721566da0779eed5c6b308b3fb7
SharedOps <manikanta.mundru@gmail.com>
2019-04-14 22:40:04 +0200

    Condition added

--- RameshTraning/Program.cs
+++ RameshTraning/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RameshUtilityLibrary.Conditions;
 
 namespace RameshTraning
 {
@@ -10,7 +11,7 @@ namespace RameshTraning
     {
         static void Main(string[] args)
         {
-
+            Condition.Test("Ramesh");
         }
     }
 }
---
+++ RameshUtilityLibrary/Conditions/Condition.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RameshUtilityLibrary.Conditions
+{
+   public static class Condition
+    {
+        public static void Test(string name)
+        {
+            if (name != null)
+            {
+                Console.WriteLine("Name is "+ name);
+            }
+
+        }
+
+    }
+}
