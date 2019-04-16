3d2fc0b209fc7de93f8fca285b9982a789556e4c
RamboMaddineni <49410680+RamboMaddineni@users.noreply.github.com>
2019-04-16 14:34:19 +0200

    Dictionart Added

---
+++ RameshUtilityLibrary/Collections/sortExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RameshUtilityLibrary.Collections
+{
+   public static class sortExample
+    {
+        public static void CreateData()
+        {
+            var stk = new Stack<string>();
+            stk.Push("Ramesh");
+            stk.Push("Sravani");
+            stk.Push("Mani");
+            stk.Push("Deepu");
+            stk.Push("Chaitra");
+            foreach (var item in stk)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
